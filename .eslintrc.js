84f750b0c35e3900a6c48e21213b344c387f26a1
Igor Cezatte <igorcezatte13@gmail.com>
2019-11-24 20:25:06 -0300

    feature: sequelize configured

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,23 @@
 module.exports = {
   env: {
-    commonjs: true,
     es6: true,
-    node: true
+    node: true,
   },
-  extends: ["airbnb-base"],
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
-    ecmaVersion: 2018
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
   rules: {
-    "class-methods-use-this": "off",
-    "no-param-reassign": "off",
-    cammelcase: "off"
-  }
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+  },
 };
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ const { Router } = require('express');
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello Igor' }));
+routes.get('/', (req, res) =>
+  res.json({ message: 'This is the challenge number 2 from adsoft' })
+);
 
 module.exports = routes;
